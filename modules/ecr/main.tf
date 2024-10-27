aff67e17ee15a207b75a884995b6fc4488d61d16
john-matlock-eng <91761832+john-matlock-eng@users.noreply.github.com>
2024-10-27 13:20:26 -0500

    tergs

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "repo" {
   name                 = var.repository_name
   image_tag_mutability = var.image_tag_mutability
+  tags                 = var.tags
 
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
